84bc8f3d3afe6da96f952b44b3c45fcc487cd0c8
Jim <hamski.2@gmail.com>
2016-04-17 21:48:51 -0500

    beginning data preparation

--- data_preparation.R
+++ data_preparation.R
@@ -1,6 +1,14 @@
 library(readxl)
 library(dplyr)
 
-microdata <- read_excel('FRBNY-SCE-Public-Microdata-Complete.xlsx', sheet = 'Data')
-column.key <- read_excel('FRBNY-SCE-Public-Microdata-Complete.xlsx', sheet = 'Codebook')
 
+
+microdata <- read_excel('FRBNY-SCE-Public-Microdata-Complete.xlsx', sheet = 'Data', na = "NA", skip = 1)
+
+microdata$date <- as.Date(microdata$date, origin = "1900-01-01", format = "%Y%m") 
+
+column.key <- read_excel('FRBNY-SCE-Public-Microdata-Complete.xlsx', sheet = 'Codebook', skip = 1)
+
+column.key <- cbind(column.key$Question, column.key$Description)
+
+#View(column.key)
