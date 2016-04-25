86f992c1dc2860cc395e400e13a5ec5ccb483b7d
jhamski <hamski.2@gmail.com>
2016-04-25 07:29:48 -0400

    data prep for three more columns

--- data_preparation.R
+++ data_preparation.R
@@ -3,18 +3,42 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 
-
-
 microdata <- read_excel('FRBNY-SCE-Public-Microdata-Complete.xlsx', sheet = 'Data', na = "NA", skip = 1)
 colnames(microdata)[189] <- "_HH_INC_CAT2"
-
-microdata$date <- as.Date(microdata$date, origin = "1900-01-01", format = "%Y%m") 
 
 column.key <- read_excel('FRBNY-SCE-Public-Microdata-Complete.xlsx', sheet = 'Codebook', skip = 1)
 
 column.key <- cbind(column.key$Question, column.key$Description)
 
 #View(column.key)
+
+
+############################
+# Data prep for Final Project
+############################
+
+microdata$date <- as.Date(microdata$date, origin = "1900-01-01", format = "%Y%m") 
+
+education.levels <- c("Less than high school",
+                      "High school dipoma/equivalent",
+                      "Some college but no degree",
+                      "Associates/ JC College degree",
+                      "Bachelor's Degree",
+                      "Master's Degree",
+                      "Doctoral Degree",
+                      "Professional Degree",
+                      "Other education")
+
+microdata$Q36 <- factor(microdata$Q36, levels = 1:9, labels = education.levels)
+
+better.worse <- c("much worse off",
+                 "somewhat worse off",
+                 "about the same",
+                 "somewhat better off",
+                 "much better off")
+
+microdata$Q1 <- factor(microdata$Q1, levels = 1:5, labels = better.worse)
+microdata$Q2 <- factor(microdata$Q2, levels = 1:5, labels = better.worse)
 
 
 ############################
@@ -35,18 +59,6 @@ d3.dataset <- microdata %>%
   sample_n(100)
 
 colnames(d3.dataset) <- c("age", "education", "income_expectation","chance_leave_job")
-
-education.levels <- c("Less than high school",
-                      "High school dipoma/equivalent",
-                      "Some college but no degree",
-                      "Associates/ JC College degree",
-                      "Bachelor's Degree",
-                      "Master's Degree",
-                      "Doctoral Degree",
-                      "Professional Degree",
-                      "Other education")
-
-d3.dataset$education <- factor(d3.dataset$education, levels = 1:9, labels = education.levels)
 
 ggplot(data = d3.dataset, aes(age, income_expectation, size = chance_leave_job)) + geom_point(aes(color=factor(education)))
 
