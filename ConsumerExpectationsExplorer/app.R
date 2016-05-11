cb036e0b0b76394e26b20afa97879514ced6ec57
jhamski <hamski.2@gmail.com>
2016-05-10 22:23:07 -0400

    Added reactive drop down element

--- ConsumerExpectationsExplorer/app.R
+++ ConsumerExpectationsExplorer/app.R
@@ -1,46 +1,72 @@
 
 library(shiny)
+library(shinythemes) #https://rstudio.github.io/shinythemes/
 library(ggplot2)
+library(dplyr)
 
 load("data_demo.Rda")
 load("data_all.Rda")
 
+# ----- UI ---------------------------------------------------> 
 
-# Define UI for application that draws a histogram
-ui <- shinyUI(fluidPage(
+ui <- shinyUI(navbarPage("NYFRB Consumer Expectations Survey",
+            tabPanel("Timeseries View", fluidPage(theme = shinytheme("Flatly"),
    
-   # Application title
-   titlePanel("NYFRB Consumer Expectations Survey"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        selectInput("survey", label = h5("Survey Question"), 
-                    choices = choices.1),
-        selectInput("question", label = h5("Survey Question for Demographic Slice"), 
-                    choices = unique(data.demo$Question)),
-        selectInput("demo", label = h5("Demographic Slice"), 
-                    choices = unique(data.demo$Demographic))
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-))
+                     # Page title
+                     titlePanel("NYFRB Consumer Expectations Survey"),
+                     
+                     sidebarLayout(
+                        sidebarPanel(
+                          selectInput("Question", label = h5("Survey Question - All Respondents"), 
+                                      choices = unique(data.all$Question)),
+                          
+                          selectInput('metric', 'Metric', ""),
+                          
+                          selectInput("question", label = h5("Survey Question for Demographic Slice"), 
+                                      choices = unique(data.demo$Question)),
+                          selectInput("demo", label = h5("Demographic Slice"), 
+                                      choices = unique(data.demo$Demographic))
+                        ),
+                        
+                        # Demo plot
+                        mainPanel(
+                           plotOutput("demoPlot")
+                        )
+                      )
+                    )
+                  ),
+            
+            
+            
+            tabPanel("More Information", fluidPage(
+                       titlePanel("test")
+                      )
+                     )
+            
+  )
+)
 
-# Define server logic required to draw a histogram
-server <- shinyServer(function(input, output) {
-   
-   output$distPlot <- renderPlot({
-     ggplot() + 
-       geom_line(data =  filter(data.all, survey == input$survey), aes(x = date, y = results, color = input$survey)) + 
-       geom_line(data =  filter(data.demo, Question == input$question & Demographic == input$demo), 
-                 aes(x = date, y = results, color = as.character(input$demo))) + 
-       scale_color_manual(values = c("blue", "red"), guide = guide_legend(title = "Timeseries Displayed")) + 
-       theme(legend.position = "bottom")
-   })
+# ----- Server ---------------------------------------------------> 
+
+server <- shinyServer(function(input, output, session) {
+            outVar = reactive({
+              mydata = unique(filter(data.all, Question == input$Question))
+              return(mydata$survey)
+            })
+            
+            observe({
+              updateSelectInput(session, "metric",
+                                choices = outVar()
+            )})
+               
+             output$demoPlot <- renderPlot({
+               ggplot() + 
+                 geom_line(data =  filter(data.all, Question == input$Question & survey == input$metric), aes(x = date, y = results, color = input$Question)) + 
+                 geom_line(data =  filter(data.demo, Question == input$question & Demographic == input$demo), 
+                           aes(x = date, y = results, color = as.character(input$demo))) + 
+                 scale_color_manual(values = c("blue", "red"), guide = guide_legend(title = "Timeseries Displayed")) + 
+                 theme(legend.position = "bottom")
+             })
 })
 
 # Run the application 
